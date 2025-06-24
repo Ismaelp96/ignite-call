4e704c81e46802014401e794b409f7cdb4776574
Ismaelp96 <ismael6991@live.com>
2025-06-24 16:40:33 -0300

    feat: instalação do Design System

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from '@ignite-ui/react';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+	return (
+		<Html lang='pt-BR'>
+			<Head>
+				<style
+					id='stitches'
+					dangerouslySetInnerHTML={{ __html: getCssText() }}
+				/>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
