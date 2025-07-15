3af1673c138487760304b189aa0c3a319f905fad
Ismaelp96 <ismael6991@live.com>
2025-07-15 12:20:19 -0300

    feat: tabela de agendamentos #8

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -8,17 +8,23 @@ import {
 	TimePickerItem,
 	TimePickerList,
 } from './styles';
+import dayjs from 'dayjs';
 
 export function CalendarStep() {
 	const [selectedDate, setSelectedDate] = useState<Date | null>(null);
 	const isDateSelected = !!selectedDate;
+
+	const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null;
+	const describedDate = selectedDate
+		? dayjs(selectedDate).format('DD[ de ]MMMM')
+		: null;
 	return (
 		<Container isTimePickerOpen={isDateSelected}>
 			<Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 			{isDateSelected && (
 				<TimePicker>
 					<TimePickerHeader>
-						Terça-feira, <span>20 de setembro</span>
+						{weekDay}, <span>{describedDate}</span>
 					</TimePickerHeader>
 					<TimePickerList>
 						<TimePickerItem>08:00h</TimePickerItem>
