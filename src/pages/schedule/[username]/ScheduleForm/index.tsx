e8ff6ae1424172c45b399b656f8036527bd65931
Ismaelp96 <ismael6991@live.com>
2025-07-22 21:49:47 -0300

    feat: fluxo da confirmação #9

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import { CalendarStep } from './CalendarStep';
 import { ConfirmStep } from './ConfirmStep';
 
 export function ScheduleForm() {
-	return <CalendarStep />;
+	const [selectedDateTime, setSelectDateTime] = useState<Date | null>();
+	function handleClearSelectedDateTime() {
+		setSelectDateTime(null);
+	}
+	if (selectedDateTime) {
+		return (
+			<ConfirmStep
+				schedulingDate={selectedDateTime}
+				onCancelConfirmation={handleClearSelectedDateTime}
+			/>
+		);
+	}
+	return <CalendarStep onSelectDateTime={setSelectDateTime} />;
 }
